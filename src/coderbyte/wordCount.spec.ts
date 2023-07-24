ef4547b186591ff43b4ad5de67f3705a4e140c3c
Eric <ericrivera228@gmail.com>
2023-07-24 12:50:20 -0600

    Formatting.

--- src/coderbyte/wordCount.spec.ts
+++ src/coderbyte/wordCount.spec.ts
@@ -1,7 +1,11 @@
 import { sum } from './wordCount';
 
-describe('sum', => {
-  it('2 + 2', () => {
-    expect(sum(2, 2).toEqual(4))
-  })
+describe('sum', () => {
+
+  it('2+2', () => {
+
+    expect(sum(2, 2)).toEqual(4);
+  
+  });
+
 });
