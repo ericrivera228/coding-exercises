13f8e123d59ef8eb78f8df0752eca518099b3ca0
Eric <ericrivera228@gmail.com>
2023-07-24 10:16:57 -0600

    Finished configuring eslint.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 if(true){
 
-	console.log('hey, listen!');
+  console.log('hey, listen!');
+
+  const test2 = { foo:'bar' };
+
+  const array1 = [ 1, 2, 3, 4 ];
+
+  const test = {
+    propA: 1,
+    propB: 2
+  };
+
+  const function1 = (a: number) => console.log(a);
 
 }
